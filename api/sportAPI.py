d6b08fa84da4f8825ccce7ce89568851e6883be8
Fabian Oraze <oraze.fabian@icloud.com>
2021-05-10 08:20:40 +0200

    added another route for detailed stats

--- api/sportAPI.py
+++ api/sportAPI.py
@@ -15,13 +15,20 @@ class SoccerStats(Resource):
         elif method == 'update':
             soccer.update_stats()
             return '', 200
-        elif method == 'detailpredict':
-            return soccer.get_details_to_prediction(args['match_ID'])
-        elif method == 'headtohead':
-            return soccer.get_head_to_head_details(args['match_ID'])
-        elif method == 'awaylastten':
-            return soccer.get_away_last_10(args['match_ID'])
-        elif method == 'homelastten':
-            return soccer.get_home_last_10(args['match_ID'])
         else:
             abort(404, message='Route not found')
+
+
+class SoccerDetails(Resource){
+    def get(self, method, match_ID):
+        if method == 'detailpredict':
+            return soccer.get_details_to_prediction(match_ID)
+        elif method == 'headtohead':
+            return soccer.get_head_to_head_details(match_ID)
+        elif method == 'awaylastten':
+            return soccer.get_away_last_10(match_ID)
+        elif method == 'homelastten':
+            return soccer.get_home_last_10(match_ID)
+        else:
+            abort(404, message='Route not')
+}
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_restful import Api
-from api.sportAPI import SoccerStats
+from api.sportAPI import SoccerStats, SoccerDetails
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,6 +12,7 @@ def welcome_server():
 
 
 api.add_resource(SoccerStats, '/api/<string:method>')
+api.add_resource(SoccerDetails, '/api/<string:method>/<int:match_ID>')
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
